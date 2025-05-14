7a34f606373789c37b71f7db86f6ff19ace333d7
ArminAustin200 <145053354+ArminAustin200@users.noreply.github.com>
2025-05-13 21:22:56 -0400

    Cleaned up Phat RGH1.2 Timings

--- Firmware/Phat/RGH1.2 Timing/output_files/FreeRunner.cdf
+++ Firmware/Phat/RGH1.2 Timing/output_files/FreeRunner.cdf
@@ -4,7 +4,7 @@ JedecChain;
 	DefaultMfr(6E);
 
 	P ActionCode(Cfg)
-		Device PartName(5M160ZE64) Path("F:/Austin/Documents/FreeRunner Modchip/FreeRunner Project (5M160Z)/Split Post Proc - Traditional Phat Timing/output_files/") File("FreeRunner.pof") MfrSpec(OpMask(1) SEC_Device(5M160ZE64) Child_OpMask(2 1 1));
+		Device PartName(5M160ZE64) Path("F:/Austin/Documents/FreeRunner Project/Firmware/Phat/RGH1.2 Timing/output_files/") File("FreeRunner.pof") MfrSpec(OpMask(1) SEC_Device(5M160ZE64) Child_OpMask(2 1 1));
 
 ChainEnd;
 
--- Firmware/Slim/RGH1.2 Timing/post_proc.vhd
+++ Firmware/Slim/RGH1.2 Timing/post_proc.vhd
@@ -1,11 +1,12 @@
--- RGH1.2 Code for Altera MAX V 5M160Z Falcon/Jasper/Tonasket Motherboards
--- 15432, GliGli, Octal450, ArminAustin200
+-- RGH1.2 Code for Matrix/Coolrunner, Trinity/Corona Version
+-- 15432, GliGli, Octal450
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
 -- main module
+
 entity post_proc is
 	Port (
 		callback : in STD_LOGIC;
@@ -19,25 +20,24 @@ end post_proc;
 
 architecture arch of post_proc is
 
-constant R_LEN : integer := 2;
-constant R_STA : integer := 349821; -- 18 - 21 best
-constant T_BUF : integer := 50000;
+constant R_LEN : integer := 12;
+constant R_STA : integer := 1749095; -- 90 - 95 best for STBY_CLK, if using oscilator adjust value as needed, check slower.vhd values
+constant T_BUF : integer := 10000;
 constant T_END : integer := R_STA + R_LEN + T_BUF;
 
-constant post_rgh : integer := 24;
-constant post_s_max : integer := 31;
-signal post_r_cnt : integer range 0 to post_s_max := 0; --Creating a constant to count all rising edges of POST
-signal post_f_cnt : integer range 0 to post_s_max := 0; --Creating a constant to count all falling edges of POST
+constant post_rgh : integer := 11;
+constant post_s_max : integer := 15;
+signal post_r_cnt : integer range 0 to post_s_max := 0; --Counter for the rising edge of the post counter
+signal post_f_cnt : integer range 0 to post_s_max := 0; --COunter for the falling edge of the post counter
 signal cnt : integer range 0 to T_END := 0;
 signal timeout : STD_LOGIC := '0';
 
 begin
 
 -- post counter
-process (POST, post_r_cnt, post_f_cnt) is
+process (POST) is
 begin
-	--Counting POST on rising edges
-	if (rising_edge(POST)) then
+		if (rising_edge(POST)) then
 		if (RST = '0') then
 			post_r_cnt <= 0;
 		else
@@ -46,11 +46,9 @@ begin
 			end if;
 		end if;
 	end if;
-	
-	--Counting POST on falling edges
 	if (falling_edge(POST)) then
-		if (RST = '0') then 
-			post_f_cnt <= 0; 
+		if (RST = '0') then
+			post_f_cnt <= 0;
 		else
 			if (post_f_cnt < post_s_max) then
 				post_f_cnt <= post_f_cnt + 1;
@@ -58,11 +56,7 @@ begin
 		end if;
 	end if;
 	
-	if (post_r_cnt + post_f_cnt < post_s_max) then
-		DBG <= POST;
-	else
-		DBG <= '0';
-	end if;
+	DBG <= POST;
 end process;
 
 -- timing counter + reset
@@ -84,11 +78,7 @@ begin
 		if (cnt >= R_STA and cnt < R_STA + R_LEN and callback = '1') then
 			RST <= '0';
 		else
-			if (cnt = R_STA + R_LEN) then
-				RST <= '1';
-			else
-				RST <= 'Z';
-			end if;
+			RST <= 'Z';
 		end if;
 	end if;
 end process;
