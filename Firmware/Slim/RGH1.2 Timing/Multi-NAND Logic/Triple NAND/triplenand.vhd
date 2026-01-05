28a6cbe0e826d964a30c6c9d9fc2c38b942be4c6
ArminAustin200 <145053354+ArminAustin200@users.noreply.github.com>
2026-01-05 12:41:52 -0500

    Reformatted Slim RGH1.2 Triple NAND Structure

--- Firmware/Slim/RGH1.2 Timing/Multi-NAND Logic/Triple NAND/triplenand.vhd
+++ Firmware/Slim/RGH1.2 Timing/Multi-NAND Logic/Triple NAND/triplenand.vhd
@@ -2,9 +2,9 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
--- dualnand module
+-- triplenand module
 
-entity dualnand is
+entity triplenand is
 	Port (
 		RST : in STD_LOGIC;
 		BUT : in STD_LOGIC;
@@ -15,9 +15,9 @@ entity dualnand is
 		SMC : out STD_LOGIC := 'Z';
 		DBG : out STD_LOGIC := '0'
 	);
-end dualnand;
+end triplenand;
 
-architecture arch of dualnand is
+architecture arch of triplenand is
 
 signal counter : integer range 0 to 10 := 0; --10 works for me
 signal counter_smc : integer range 0 to 1 := 0;
