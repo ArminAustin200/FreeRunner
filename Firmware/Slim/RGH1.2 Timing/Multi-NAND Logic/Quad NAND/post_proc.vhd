7822c45b6e276a991d6ebf1e4357080c728b1588
ArminAustin200 <145053354+ArminAustin200@users.noreply.github.com>
2026-01-05 13:07:12 -0500

    Reformatted Slim RGH1.2 Quad NAND Structure

--- Firmware/Slim/RGH1.2 Timing/Multi-NAND Logic/Quad NAND/post_proc.vhd
+++ Firmware/Slim/RGH1.2 Timing/Multi-NAND Logic/Quad NAND/post_proc.vhd
@@ -31,7 +31,6 @@ constant post_s_max : integer := 15;
 signal post_r_cnt : integer range 0 to post_s_max := 0; --Creating a constant to count all rising edges of POST
 signal post_f_cnt : integer range 0 to post_s_max := 0; --Creating a constant to count all falling edges of POST
 signal cnt : integer range 0 to T_END := 0;
-signal post_cnt : integer range 0 to post_s_max := 0;
 signal timeout : STD_LOGIC := '0';
 
 begin
@@ -91,7 +90,7 @@ begin
 end process;
 
 -- slowdown
-process (post_cnt, timeout) is
+process (post_r_cnt, post_f_cnt, timeout) is
 begin
 	if (post_r_cnt + post_f_cnt >= post_rgh - 1 and timeout = '0') then
 		to_slow <= '1';
